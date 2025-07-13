d6cfcf6670ebed45384394240e85a56643725074
Glody Mafo <glodymafokis@gmail.com>
2025-07-13 14:30:35 +0200

    week01 finished

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require ('express');
+const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use('/', require('./routes'));
 
-app.listen(port , ()=>{
-    console.log(`runing on port ${port}`);
-    
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+
+    else {
+        app.listen(port, () => {
+            console.log(`database is listened and server runing on port ${port}`);
+
+        })
+    }
 })
+
+
